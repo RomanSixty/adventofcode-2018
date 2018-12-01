97c6e3b5fc0e313ecfa925d5d561fe6c767c6f06
Alexander Schulze <github@lxhome.de>
2018-12-01 17:36:56 +0100

    Day 01

---
+++ 01.php
@@ -0,0 +1,33 @@
+<?php
+
+$input = file ( '01.txt' );
+
+$output = 0;
+
+// part 1
+
+foreach ( $input as $i )
+	$output += $i;
+
+echo 'First Part: ' . $output . "\n";
+
+// part 2
+
+$i      = 0;
+$output = 0;
+$freqs  = [ 0 => true ];
+$size   = count ( $input );
+
+while ( true )
+{
+	$output += $input [ $i % $size ];
+
+	if ( isset ( $freqs [ $output ] ) )
+		break;
+
+	$freqs [ $output ] = true;
+
+	$i++;
+}
+
+echo 'Second Part: ' . $output . "\n";
---
+++ README.md
@@ -0,0 +1,10 @@
+# Advent of Code 2018
+http://adventofcode.com/2018
+
+## Usage
+
+Each day is numbered and has a *.txt file for input data and a *.php file for processing.
+
+Run using php-cli:
+
+`php 01.php`
