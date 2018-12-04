04be042a0d4d8d9bd163fbb378de358611758dc2
Alexander Schulze <github@lxhome.de>
2018-12-04 21:09:03 +0100

    Day 04

---
+++ 04/solve.php
@@ -0,0 +1,98 @@
+<?php
+
+$input = file ( __DIR__ . '/input' );
+
+$current_guard = null;
+$sleep_start   = false;
+$guards        = [];
+
+sort ( $input );
+
+// parse guard log into a multidimensional array
+// guard_id => minute after midnight => number of times asleep
+
+foreach ( $input as $logentry )
+{
+    $matches = [];
+
+    preg_match ( '~(\d+):(\d+)\] (wakes up|falls asleep|Guard #(\d+) begins shift)~', $logentry, $matches );
+
+    // new guard entry
+    if ( count ( $matches ) == 5 )
+    {
+        $current_guard = $matches [ 4 ];
+        continue;
+    }
+
+    // sleep or wakeup time of current guard
+    else
+    {
+        if ( $matches [ 3 ] == 'falls asleep' )
+        {
+            $sleep_start = (int) $matches [ 2 ];
+        }
+        else // 'wakes up'
+        {
+            for ( $sleep_minute = $sleep_start; $sleep_minute < $matches [ 2 ]; $sleep_minute++ )
+            {
+                if ( !isset ( $guards [ $current_guard ][ $sleep_minute ] ) )
+                    $guards [ $current_guard ][ $sleep_minute ] = 0;
+
+                $guards [ $current_guard ][ $sleep_minute ]++;
+            }
+
+            $sleep_start = false;
+        }
+    }
+}
+
+// this time we solve both parts in the same loop
+
+$max_minutes       = 0; // max minutes a single guard was asleep
+$minute_slept_most = 0; // which minute was slept the most
+
+$p1_chosen_guard  = null;
+$p1_chosen_minute = null;
+$p2_chosen_guard  = null;
+$p2_chosen_minute = null;
+
+foreach ( $guards as $guard_id => $minutes )
+{
+    $sum_minutes        = 0; // how long did the current guard sleep?
+    $most_asleep_times  = 0; // how often was slept in a minute?
+    $most_asleep_minute = null; // which minute is record holder for most naps?
+
+    foreach ( $minutes as $m => $count )
+    {
+        // part 1
+
+        $sum_minutes += $count;
+
+        if ( $count > $most_asleep_times )
+        {
+            $most_asleep_minute = $m;
+            $most_asleep_times = $count;
+        }
+
+        // part 2
+
+        if ( $count > $minute_slept_most )
+        {
+            $minute_slept_most = $count;
+            $p2_chosen_minute  = $m;
+            $p2_chosen_guard   = $guard_id;
+        }
+    }
+
+    // part 1
+
+    if ( $max_minutes < $sum_minutes )
+    {
+        $max_minutes      = $sum_minutes;
+        $p1_chosen_minute = $most_asleep_minute;
+        $p1_chosen_guard  = $guard_id;
+    }
+}
+
+echo 'First Part: ' . $p1_chosen_guard * $p1_chosen_minute . "\n";
+echo 'Second Part: ' . $p2_chosen_guard * $p2_chosen_minute . "\n";
