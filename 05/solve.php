f993af9c56c4ccde2666cb0387adca3ea6798983
Alexander Schulze <github@lxhome.de>
2018-12-05 20:59:14 +0100

    Day 05

---
+++ 05/solve.php
@@ -0,0 +1,35 @@
+<?php
+
+$input = file_get_contents ( __DIR__ . '/input' );
+
+// first time a function is used in this advent of code
+function reactPolymer ( $polymer )
+{
+    $search = str_split ( 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZZzYyXxWwVvUuTtSsRrQqPpOoNnMmLlKkJjIiHhGgFfEeDdCcBbAa', 2 );
+
+	do
+	{
+		$lastLength = strlen ( $polymer );
+
+	    $polymer = str_replace ( $search, '', $polymer );
+	}
+	while ( strlen ( $polymer ) < $lastLength );
+
+	return $lastLength;
+}
+
+$shortest = reactPolymer ( $input );
+
+echo 'First Part: ' . $shortest . "\n";
+
+foreach ( str_split ( 'abcdefghijklmnopqrstuvwxyz' ) as $unittype )
+{
+	$testPolymer = str_ireplace ( $unittype, '', $input );
+
+	$reduces = reactPolymer ( $testPolymer );
+
+	if ( $reduces < $shortest )
+		$shortest = $reduces;
+}
+
+echo 'Second Part: ' . $shortest . "\n";
